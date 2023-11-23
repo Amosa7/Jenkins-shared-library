8558451b35c305f0dd67a9515db773baa6139cb6
Amosa Bashir <136304747+Amosa7@users.noreply.github.com>
2023-11-23 19:29:19 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo "building docker image of the appliction docker Image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh "docker build -t amosa77/demo-app:jma-2.5 ."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo2', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "docker build -t amosa77/demo-app:njma-3.5 ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push amosa77/demo-app:jma-2.5"
+        sh "docker push amosa77/demo-app:njma-3.5"
     }
 }
